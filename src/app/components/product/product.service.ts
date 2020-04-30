5e869e1973740575fa98eab84460ad987783e402
Estevão Marconi Rodrigues <estevao.rod@gmail.com>
2020-04-30 17:42:54 -0300

    inicial

---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,54 @@
+import { Product } from './product.model';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl: string = 'http://localhost:3001/products'
+
+  constructor(private snackBar: MatSnackBar,
+    private http: HttpClient) { }
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+    });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e : any) : Observable<any>{
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+
+  update(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product);
+  }
+
+  delete(id: string): Observable<Product> {
+    return this.http.delete<Product>(`${this.baseUrl}/${id}`);
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  readById(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
+}
